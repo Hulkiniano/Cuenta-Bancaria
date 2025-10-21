8697f6d1e8f0a12cc41c86d5f84a2e84501f491d
DarkTrrex <miguelopez271105@gmail.com>
2025-10-21 02:45:40 +0200

    Terminamos primera version de la interfaz de la cuenta bancaria

---
+++ src/cuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuentaBancaria;
+
+/**
+ *
+ * @author tarde
+ */
+public class CuentaBancaria {
+    public static void main(String[] args) {
+        InicioSesion is = new InicioSesion();
+        is.quitarOpacidad();
+        is.setVisible(true);
+    }
+}
